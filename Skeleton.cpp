aab7aa3a42bb618235ea3cee2792874b3d71f4f7
balintkiraly <kiraly.balint@simonyi.bme.hu>
2020-04-01 14:56:08 +0200

    Remove unnecessary code

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -59,113 +59,35 @@ const char* const fragmentSource = R"(
     }
 )";
 
-GPUProgram gpuProgram; // vertex and fragment shaders
-unsigned int vao;      // virtual world on the GPU
+GPUProgram gpuProgram;
+unsigned int vao;
 
-// Initialization, create an OpenGL context
 void onInitialization()
 {
     glViewport(0, 0, windowWidth, windowHeight);
-
-    glGenVertexArrays(1, &vao); // get 1 vao id
-    glBindVertexArray(vao);     // make it active
-
-    unsigned int vbo;      // vertex buffer object
-    glGenBuffers(1, &vbo); // Generate 1 buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    // Geometry with 24 bytes (6 floats or 3 x 2 coordinates)
-    float vertices[] = {-0.8f, -0.8f, -0.6f, 1.0f, 0.8f, -0.2f};
-    glBufferData(GL_ARRAY_BUFFER,  // Copy to GPU target
-                 sizeof(vertices), // # bytes
-                 vertices,         // address
-                 GL_STATIC_DRAW);  // we do not change later
-
-    glEnableVertexAttribArray(0);                // AttribArray 0
-    glVertexAttribPointer(0,                     // vbo -> AttribArray 0
-                          2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
-                          0, NULL);              // stride, offset: tightly packed
-
-    // create program for the GPU
+    
     gpuProgram.create(vertexSource, fragmentSource, "outColor");
 }
 
-// Window has become invalid: Redraw
 void onDisplay()
 {
-    glClearColor(0, 0, 0, 0);     // background color
-    glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    // Set color to (0, 1, 0) = green
-    int location = glGetUniformLocation(gpuProgram.getId(), "color");
-    glUniform3f(location, 0.0f, 1.0f, 0.0f); // 3 floats
-
-    float MVPtransf[4][4] = {1, 0, 0, 0, // MVP matrix,
-                             0, 1, 0, 0, // row-major!
-                             0, 0, 1, 0, 0, 0, 0, 1};
-
-    location = glGetUniformLocation(gpuProgram.getId(), "MVP"); // Get the GPU location of uniform variable MVP
-    glUniformMatrix4fv(location, 1, GL_TRUE,
-                       &MVPtransf[0][0]); // Load a 4x4 row-major float matrix to the specified location
-
-    glBindVertexArray(vao); // Draw call
-    glDrawArrays(GL_TRIANGLES, 0 /*startIdx*/, 3 /*# Elements*/);
-
-    glutSwapBuffers(); // exchange buffers for double buffering
+    glutSwapBuffers();
 }
 
-// Key of ASCII code pressed
-void onKeyboard(unsigned char key, int pX, int pY)
-{
-    if (key == 'd')
-        glutPostRedisplay(); // if d, invalidate display, i.e. redraw
-}
-
-// Key of ASCII code released
-void onKeyboardUp(unsigned char key, int pX, int pY) {}
-
-// Move mouse with key pressed
-void onMouseMotion(int pX, int pY)
-{ // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-    // Convert to normalized device space
-    float cX = 2.0f * pX / windowWidth - 1; // flip y axis
-    float cY = 1.0f - 2.0f * pY / windowHeight;
-    printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
-}
-
-// Mouse click event
 void onMouse(int button, int state, int pX, int pY)
-{ // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-    // Convert to normalized device space
-    float cX = 2.0f * pX / windowWidth - 1; // flip y axis
+{ 
+    float cX = 2.0f * pX / windowWidth - 1;
     float cY = 1.0f - 2.0f * pY / windowHeight;
 
-    char* buttonStat;
-    switch (state)
-    {
-    case GLUT_DOWN:
-        buttonStat = "pressed";
-        break;
-    case GLUT_UP:
-        buttonStat = "released";
-        break;
-    }
+    if (state == GLUT_UP && button == GLUT_LEFT_BUTTON) 
+        printf("click at (%3.2f, %3.2f)\n", cX, cY);
 
-    switch (button)
-    {
-    case GLUT_LEFT_BUTTON:
-        printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);
-        break;
-    case GLUT_MIDDLE_BUTTON:
-        printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);
-        break;
-    case GLUT_RIGHT_BUTTON:
-        printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);
-        break;
-    }
 }
 
-// Idle event indicating that some time elapsed: do animation here
-void onIdle()
-{
-    long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
-}
+void onKeyboard(unsigned char key, int pX, int pY) {}
+void onKeyboardUp(unsigned char key, int pX, int pY) {}
+void onMouseMotion(int pX, int pY) {}
+void onIdle() {}
