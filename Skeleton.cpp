d885ae72a46911828ce6adc765af0fbce060109f
balintkiraly <kiraly.balint@simonyi.bme.hu>
2020-04-02 07:26:47 +0200

    Fix code formating

--- Skeleton.cpp
+++ Skeleton.cpp
@@ -63,47 +63,46 @@ GPUProgram gpuProgram;
 unsigned int vao;
 const static int tessellationLevel = 500;
 
-class Circle {
+class Circle
+{
     const static int numberOfVertices = tessellationLevel;
     unsigned int vao, vbo;
     vec2 vertices[numberOfVertices];
+
 public:
-    void Create(vec2 center, float radius, float from = 0.0f, float length = 2.0f * M_PI ) {
+    void Create(vec2 center, float radius, float from = 0.0f, float length = 2.0f * M_PI)
+    {
         glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
 
         glGenBuffers(1, &vbo);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-        for(int i = 0; i < numberOfVertices; i++) {
+        for (int i = 0; i < numberOfVertices; i++)
+        {
             float fi = from + i * length / numberOfVertices;
             vertices[i] = vec2(cosf(fi) * radius, sinf(fi) * radius) + center;
         }
-        
+
         glBufferData(GL_ARRAY_BUFFER, sizeof(vec2) * numberOfVertices, vertices, GL_STATIC_DRAW);
 
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, reinterpret_cast<void*>(0)); 		     // stride, offset: tightly packed
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2,
+                              reinterpret_cast<void*>(0)); // stride, offset: tightly packed
     }
 
-    void Draw(GLenum mode, vec3 color) {
+    void Draw(GLenum mode, vec3 color)
+    {
         int location = glGetUniformLocation(gpuProgram.getId(), "color");
-        glUniform3f(location, color.x, color.y, color.z); 
-        float MVPtransf[4][4] = {
-                            1, 0, 0, 0,
-                            0, 1, 0, 0,
-                            0, 0, 1, 0,
-                            0, 0, 0, 1};
+        glUniform3f(location, color.x, color.y, color.z);
+        float MVPtransf[4][4] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
 
-        location = glGetUniformLocation(gpuProgram.getId(), "MVP"); 
+        location = glGetUniformLocation(gpuProgram.getId(), "MVP");
         glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);
         glBindVertexArray(vao);
         glDrawArrays(mode, 0, numberOfVertices);
     }
-    vec2* GetVertices()
-    {
-        return vertices;
-    }
+    vec2* GetVertices() { return vertices; }
 };
 
 class SiriusTriangle
@@ -115,33 +114,23 @@ class SiriusTriangle
     unsigned int vao, vbo;
     vec2 vertices[3 * tessellationLevel + 1];
 
-    vec2 FindCircleCenter(vec2 p1, vec2 p2) 
+    vec2 FindCircleCenter(vec2 p1, vec2 p2)
     {
         vec2 c;
-        c.y = (
-                0.5f - 
-                (p2.x + p1.y * p1.y * p2.x) / (2.0f * p1.x) + 
-                (p2.x * p2.x + p2.y * p2.y - p1.x * p2.x) / 2.0f
-              ) / (p2.y - p1.y * p2.x / p1.x);
-        c.x =  (1.0f + p1.y*p1.y) / (2.0f * p1.x) + p1.x / 2.0f - (p1.y * c.y) / p1.x;
+        c.y = (0.5f - (p2.x + p1.y * p1.y * p2.x) / (2.0f * p1.x) + (p2.x * p2.x + p2.y * p2.y - p1.x * p2.x) / 2.0f) /
+              (p2.y - p1.y * p2.x / p1.x);
+        c.x = (1.0f + p1.y * p1.y) / (2.0f * p1.x) + p1.x / 2.0f - (p1.y * c.y) / p1.x;
         return c;
     }
-    float CalculateCircleRadius(vec2 center)
-    {
-        return sqrt(length(center)*length(center)-1.0f);
-    }
-    float ChooseFromRadian(float a, float b)
-    {
-        return fabs(a-b) > M_PI ? b : a;
-    }
-    float ChooseToRadian(float a, float b)
-    {
-        return fabs(a-b) > M_PI ? a : b;
-    }
+    float CalculateCircleRadius(vec2 center) { return sqrt(length(center) * length(center) - 1.0f); }
+    float ChooseFromRadian(float a, float b) { return fabs(a - b) > M_PI ? b : a; }
+    float ChooseToRadian(float a, float b) { return fabs(a - b) > M_PI ? a : b; }
+
 public:
     void AddPoint(float cX, float cY)
     {
-        if(numberOfPoints < 3) {
+        if (numberOfPoints < 3)
+        {
             points[numberOfPoints++] = vec2(cX, cY);
         }
         if (numberOfPoints == 3 && !calculated)
@@ -151,9 +140,9 @@ public:
             vec2 B = points[1];
             vec2 C = points[2];
 
-            vec2 aCenter = FindCircleCenter(A,B);
-            vec2 bCenter = FindCircleCenter(B,C);
-            vec2 cCenter = FindCircleCenter(C,A);
+            vec2 aCenter = FindCircleCenter(A, B);
+            vec2 bCenter = FindCircleCenter(B, C);
+            vec2 cCenter = FindCircleCenter(C, A);
 
             float aRadius = CalculateCircleRadius(aCenter);
             float bRadius = CalculateCircleRadius(bCenter);
@@ -164,10 +153,10 @@ public:
 
             float bFi1 = atan2(C.y - bCenter.y, C.x - bCenter.x);
             float bFi2 = atan2(B.y - bCenter.y, B.x - bCenter.x);
-            
-            float cFi1 =  atan2(A.y - cCenter.y, A.x - cCenter.x);
-            float cFi2 =  atan2(C.y - cCenter.y, C.x - cCenter.x);
-            
+
+            float cFi1 = atan2(A.y - cCenter.y, A.x - cCenter.x);
+            float cFi2 = atan2(C.y - cCenter.y, C.x - cCenter.x);
+
             float aFrom = ChooseFromRadian(aFi1, aFi2);
             float aTo = ChooseToRadian(aFi1, aFi2);
             float bFrom = ChooseFromRadian(bFi1, bFi2);
@@ -179,60 +168,58 @@ public:
             circles[1].Create(bCenter, bRadius, bFrom, (bTo - bFrom));
             circles[2].Create(cCenter, cRadius, cFrom, (cTo - cFrom));
 
-            printf("Az 'a' oldal hossza: %3.2f\n", aRadius*fabs(aTo - aFrom));
-            printf("A 'b' oldal hossza: %3.2f\n", aRadius*fabs(bTo - bFrom));
-            printf("A 'c' oldal hossza: %3.2f\n", aRadius*fabs(cTo - cFrom));
+            printf("Az 'a' oldal hossza: %3.2f\n", aRadius * fabs(aTo - aFrom));
+            printf("A 'b' oldal hossza: %3.2f\n", aRadius * fabs(bTo - bFrom));
+            printf("A 'c' oldal hossza: %3.2f\n", aRadius * fabs(cTo - cFrom));
+
         }
     }
 
     void Draw()
     {
         vec2 centerOfTriangle = vec2(0.0f, 0.0f);
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
             centerOfTriangle.x = centerOfTriangle.x + points[i].x;
             centerOfTriangle.y = centerOfTriangle.y + points[i].y;
         }
         centerOfTriangle.x = centerOfTriangle.x / 3;
         centerOfTriangle.y = centerOfTriangle.y / 3;
-        
+
         vertices[0] = centerOfTriangle;
-        for(int n = 0; n < 3; n++)
+        for (int n = 0; n < 3; n++)
         {
             vec2* aCircleVertices = new vec2[tessellationLevel];
             aCircleVertices = circles[n].GetVertices();
             int j = 0;
-            for(int i = n * tessellationLevel + 1; i <= (n + 1) * tessellationLevel; i++ && j++)
+            for (int i = n * tessellationLevel + 1; i <= (n + 1) * tessellationLevel; i++ && j++)
             {
-                vertices[i] = aCircleVertices[tessellationLevel-j];
+                vertices[i] = aCircleVertices[tessellationLevel - j];
             }
         }
-        
+
         glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
 
         glGenBuffers(1, &vbo);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        
+
         glBufferData(GL_ARRAY_BUFFER, sizeof(vec2) * 3 * tessellationLevel + 1, vertices, GL_STATIC_DRAW);
 
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, reinterpret_cast<void*>(0)); 		     // stride, offset: tightly packed
-    
-        int location = glGetUniformLocation(gpuProgram.getId(), "color");
-        glUniform3f(location, 0.0f, 1.0f, 0.0f); 
-        float MVPtransf[4][4] = {
-                            1, 0, 0, 0,
-                            0, 1, 0, 0,
-                            0, 0, 1, 0,
-                            0, 0, 0, 1};
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2,
+                              reinterpret_cast<void*>(0)); // stride, offset: tightly packed
 
-        location = glGetUniformLocation(gpuProgram.getId(), "MVP"); 
+        int location = glGetUniformLocation(gpuProgram.getId(), "color");
+        glUniform3f(location, 0.0f, 1.0f, 0.0f);
+        float MVPtransf[4][4] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+
+        location = glGetUniformLocation(gpuProgram.getId(), "MVP");
         glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLE_FAN, 0, 3 * tessellationLevel + 1);
-        
-        for(int i = 0; i < 3; i++)
+
+        for (int i = 0; i < 3; i++)
         {
             circles[i].Draw(GL_LINE_STRIP, vec3(1.0f, 0.0f, 0.0f));
         }
@@ -245,7 +232,7 @@ SiriusTriangle siriusTriangle;
 void onInitialization()
 {
     glViewport(0, 0, windowWidth, windowHeight);
-    unitCircle.Create(vec2(0.0f,0.0f), 1.0f);
+    unitCircle.Create(vec2(0.0f, 0.0f), 1.0f);
     gpuProgram.create(vertexSource, fragmentSource, "outColor");
 }
 
@@ -259,11 +246,11 @@ void onDisplay()
 }
 
 void onMouse(int button, int state, int pX, int pY)
-{ 
+{
     float cX = 2.0f * pX / windowWidth - 1;
     float cY = 1.0f - 2.0f * pY / windowHeight;
 
-    if (state == GLUT_UP && button == GLUT_LEFT_BUTTON) 
+    if (state == GLUT_UP && button == GLUT_LEFT_BUTTON)
     {
         siriusTriangle.AddPoint(cX, cY);
         glutPostRedisplay();
